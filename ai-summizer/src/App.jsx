ae86ea8947c1c18f902a63ff27bf0857421f6b9a
KARTIKEY-KATYAL <107783109+KARTIKEY-KATYAL@users.noreply.github.com>
2024-12-04 22:37:22 +0530

    c-2

--- ai-summizer/src/App.jsx
+++ ai-summizer/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 function App() {
   return (
     <div>
-      App 
+      <Hero/>
+      <Demo/> 
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily:{
-      satoshi : ["Satoshi","sans-serif"],
-      inter : ["Inter","sans-serif"],
+    fontFamily: {
+      satoshi: ["Satoshi", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
     },
     extend: {},
   },
